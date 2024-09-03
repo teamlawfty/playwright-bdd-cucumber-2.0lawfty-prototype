4fb14ebe9cbabe0bb7285d395c980f185d3d2465
Martin Lichev <martin@lawfty.com>
2024-09-03 22:50:48 +0300

    Implemented step definition generation script, laying prototype foundations and updated imports

---
+++ step-definitions/Sign-In.steps.ts
@@ -0,0 +1,107 @@
+
+import { Given, When, Then } from '@cucumber/cucumber';
+
+    Given('I am a user who needs to sign in', async () => {
+      // Implement this step
+    });
+  
+
+    When('I navigate to the sign-in URL "/sign-in"', async () => {
+      // Implement this step
+    });
+  
+
+    Then('I should see the sign-in page with the fields "Email address" and "Password"', async () => {
+      // Implement this step
+    });
+  
+
+    Then('I should see a "Remember me" checkbox', async () => {
+      // Implement this step
+    });
+  
+
+    Then('I should see a "Sign in" button', async () => {
+      // Implement this step
+    });
+  
+
+    Then('I should see a "Forgot your password?" link', async () => {
+      // Implement this step
+    });
+  
+
+    Given('I am on the sign-in page', async () => {
+      // Implement this step
+    });
+  
+
+    When('I enter a valid email address in the "Email address" field', async () => {
+      // Implement this step
+    });
+  
+
+    When('I enter a valid password in the "Password" field', async () => {
+      // Implement this step
+    });
+  
+
+    When('I check the "Remember me" checkbox', async () => {
+      // Implement this step
+    });
+  
+
+    When('I click the "Sign in" button', async () => {
+      // Implement this step
+    });
+  
+
+    Then('I should be signed in successfully', async () => {
+      // Implement this step
+    });
+  
+
+    Then('the application should store the authentication token', async () => {
+      // Implement this step
+    });
+  
+
+    Given('I am on the sign-in page', async () => {
+      // Implement this step
+    });
+  
+
+    When('I press the "Tab" key', async () => {
+      // Implement this step
+    });
+  
+
+    Then('the focus should move sequentially through the "Email address", "Password",', async () => {
+      // Implement this step
+    });
+  
+
+    Given('I am on the sign-in page', async () => {
+      // Implement this step
+    });
+  
+
+    Given('I have entered a valid email and password', async () => {
+      // Implement this step
+    });
+  
+
+    When('I press the "Enter" key', async () => {
+      // Implement this step
+    });
+  
+
+    Then('the form should be submitted', async () => {
+      // Implement this step
+    });
+  
+
+    Then('I should be signed in successfully', async () => {
+      // Implement this step
+    });
+  
