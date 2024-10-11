cb677bf01ad9372a59bc9d4f5552dba3775a507a
Martin Lichev <martin@lawfty.com>
2024-10-12 00:01:28 +0300

    installing husky, refactoring eslint, prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,87 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import prettierConfig from 'eslint-config-prettier'
-import prettierPlugin from 'eslint-plugin-prettier'
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import react from 'eslint-plugin-react';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-
-  // Disable ESLint rules that conflict with Prettier
-  prettierConfig,
-
-  // Enable Prettier rules as ESLint rules
   {
-    ignores: ['node_modules/**', 'dist/**', 'build/**'],
+    ignores: [
+      'src/graphql/codegen/graphql.ts',
+      '.storybook/',
+      'dist/',
+      'storybook-static/',
+      'public/new-relic-agent-*.js',
+    ],
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'prettier',
+    ),
+  ),
+  {
     plugins: {
-      prettier: prettierPlugin,
+      '@typescript-eslint': fixupPluginRules(typescriptEslint),
+      react: fixupPluginRules(react),
     },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+
+    settings: {
+      react: {
+        createClass: 'createReactClass',
+        pragma: 'React',
+        fragment: 'Fragment',
+        version: 'detect',
+        flowVersion: '0.53',
+      },
+    },
+
     rules: {
-      'prettier/prettier': 'error', // You can set this to "warn" if preferred
-      'no-unused-vars': [
-        'error',
-        {
-          vars: 'all',
-          args: 'after-used',
-          ignoreRestSiblings: false,
-          varsIgnorePattern: '^_',
-        },
-      ],
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/no-children-prop': 'off',
+      'react/no-unescaped-entities': 'off',
+      'no-console': 1,
+      // Allow unused variables that start with an underscore
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     },
   },
-]
+  {
+    files: ['**/.eslintrc.{js,cjs}'],
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      ecmaVersion: 5,
+      sourceType: 'commonjs',
+    },
+  },
+];
--- step-definitions/generic.steps.ts
+++ step-definitions/generic.steps.ts
@@ -86,6 +86,7 @@ When(/^I (check|uncheck) the "(.+)" checkbox$/, async (action: string, label: st
 Then(/^I should see the "(.+)" (field|button|checkbox|link|textarea)$/, async (label: string, elementType: string) => {
   const page = getPage();
   let locators: string[] = []; // Array to hold possible locators
+  // test
 
   switch (elementType) {
     case 'field':
